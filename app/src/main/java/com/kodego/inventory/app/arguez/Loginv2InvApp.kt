8a241659bb100c304ec0ab41731b5a602c999433
GinPaul <luginlpp@gmail.com>
2022-10-18 23:17:55 +0800

    Oct 18 2022 commit

---
+++ app/src/main/java/com/kodego/inventory/app/arguez/Loginv2InvApp.kt
@@ -0,0 +1,11 @@
+package com.kodego.inventory.app.arguez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Loginv2InvApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginv2_inv_app)
+    }
+}
